da5c08733c17ce66d373a5296039a0cbef312d73
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-08-05 13:18:45 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python
-""" holds class Amenity"""
+"""Holds the class Amenity"""
+
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
+    """Representation of Amenity"""
     if models.storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
@@ -17,5 +14,5 @@ class Amenity(BaseModel, Base):
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
+        """Initializes Amenity"""
         super().__init__(*args, **kwargs)
