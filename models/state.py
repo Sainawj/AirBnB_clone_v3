64f01c9640bc4b5c29f6c8725aaadf83ebf6b9be
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-08-05 13:07:41 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
-"""Holds class State"""
+""" holds class State"""
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Representation of state"""
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initializes state"""
+        """initializes state"""
         super().__init__(*args, **kwargs)
 
-    @property
-    def cities(self):
-        """Getter for list of city instances related to the state"""
-        from models import storage  # Delayed import
-        city_list = []
-        all_cities = storage.all(City)
-        for city in all_cities.values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
