28aefacb7e7cbc6c8481844155a75cbcba7807af
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-08-05 13:31:24 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,21 @@
-#!/usr/bin/python3
-"""Create a storage instance based on the environment."""
-
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 import os
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
-# Initialize storage based on the environment
-def get_storage():
-    """Return the storage instance."""
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        return DBStorage()
-    else:
-        return FileStorage()
+"""CNC - dictionary = { Class Name (string) : Class Type }"""
 
-storage = get_storage()
+if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine import db_storage
+    CNC = db_storage.DBStorage.CNC
+    storage = db_storage.DBStorage()
+else:
+    from models.engine import file_storage
+    CNC = file_storage.FileStorage.CNC
+    storage = file_storage.FileStorage()
+
 storage.reload()
