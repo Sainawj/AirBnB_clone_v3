1563f9532f427b452fdc19cdf49eda109c954422
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-08-05 13:06:12 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,24 +1,26 @@
-#!/usr/bin/python3
-"""City Module for HBNB project."""
-import os
+#!/usr/bin/python
+""" holds class City"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel, Base
 
 class City(BaseModel, Base):
-    """The city class, containing state ID and name."""
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    places = relationship(
-        'Place',
-        cascade='all, delete, delete-orphan',
-        backref='cities'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place",
+                              backref="cities",
+                              cascade="all, delete, delete-orphan")
+    else:
+        state_id = ""
+        name = ""
 
-class Place(BaseModel, Base):
-    """Representation of a place"""
-    __tablename__ = 'places'
-    id = Column(String(60), primary_key=True, nullable=False)
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
